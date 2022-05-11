e3244c177c71a09f6889ea0adff9d4dea32ef84b
Dipak 2056 <paudelsantosh508@gmail.com>
2022-05-11 16:41:10 +1000

    one user card is done

--- src/user/pages/Users.jsx
+++ src/user/pages/Users.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { UserList } from "../components/UserList";
 export const Users = () => {
   const USERS = [
-    // {
-    //   id: "u1",
-    //   name: "dipak paudel",
-    //   image:
-    //     "https://image.shutterstock.com/image-photo/smiling-bearded-young-male-model-600w-788313199.jpg",
-    //   places: 3,
-    // },
+    {
+      id: "u1",
+      name: "dipak paudel",
+      image:
+        "https://image.shutterstock.com/image-photo/smiling-bearded-young-male-model-600w-788313199.jpg",
+      places: 3,
+    },
   ];
   return <UserList items={USERS} />;
 };
