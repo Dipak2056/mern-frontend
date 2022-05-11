83f2bb42cb345fdf998706633c44dead27c84a92
Dipak 2056 <paudelsantosh508@gmail.com>
2022-05-11 19:32:25 +1000

    routing acrdn toquery params is done successfully

--- src/places/pages/UserPlaces.jsx
+++ src/places/pages/UserPlaces.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
 import { PlaceList } from "../components/PlaceList";
 
 const DUMMY_PLACES = [
@@ -30,5 +32,7 @@ const DUMMY_PLACES = [
   },
 ];
 export const UserPlaces = () => {
-  return <PlaceList items={DUMMY_PLACES}></PlaceList>;
+  const userId = useParams().uid;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPlaces}></PlaceList>;
 };
