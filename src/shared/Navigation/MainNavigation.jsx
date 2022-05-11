3bc2f298d0ad9a1e04fff4facb99e5f1a42ad9d8
Dipak 2056 <paudelsantosh508@gmail.com>
2022-05-11 17:20:51 +1000

    nav links are added

--- src/shared/Navigation/MainNavigation.jsx
+++ src/shared/Navigation/MainNavigation.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 import { Mainheader } from "./Mainheader";
+import { NavLinks } from "./NavLinks";
 
 import "./MainNavigation.css";
 
@@ -16,7 +17,9 @@ export const MainNavigation = (props) => {
       <h1 className="main-navigation__title">
         <Link to="/">Your Places</Link>
       </h1>
-      <nav>...</nav>
+      <nav>
+        <NavLinks />
+      </nav>
     </Mainheader>
   );
 };
