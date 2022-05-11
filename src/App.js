535f508964e26e9f50d8ee172b5ccc565b59c2b0
Dipak 2056 <paudelsantosh508@gmail.com>
2022-05-11 16:09:50 +1000

    worked in userpage components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Users } from "./user/pages/Users";
+import { NewPlaces } from "./places/pages/NewPlaces";
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Users />}></Route>
+        <Route path="/places/new" exact element={<NewPlaces />}></Route>
+        <Route path="*" exact element={<Users />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
