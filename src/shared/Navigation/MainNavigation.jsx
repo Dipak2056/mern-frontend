4ed62b72f9872e42637558c9bd79c96f1c73a2de
Dipak 2056 <paudelsantosh508@gmail.com>
2022-05-11 17:49:03 +1000

    backdrop and the hamburger menu rendering done

--- src/shared/Navigation/MainNavigation.jsx
+++ src/shared/Navigation/MainNavigation.jsx
@@ -1,25 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Link } from "react-router-dom";
 import { Mainheader } from "./Mainheader";
 import { NavLinks } from "./NavLinks";
-
+import { SideDrawer } from "./SideDrawer";
 import "./MainNavigation.css";
+import { Backdrop } from "../components/UIElements/Backdrop";
 
 export const MainNavigation = (props) => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+  const openDrawer = () => {
+    setDrawerIsOpen(true);
+  };
+  const closeDrawer = () => {
+    setDrawerIsOpen(false);
+  };
+
   return (
-    <Mainheader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">Your Places</Link>
-      </h1>
-      <nav>
-        <NavLinks />
-      </nav>
-    </Mainheader>
+    <>
+      {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
+      {drawerIsOpen && (
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      )}
+      <Mainheader>
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">Your Places</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </Mainheader>
+    </>
   );
 };
